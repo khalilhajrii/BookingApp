542a2a1a5c2ab03ecf5b6cce0d883150d2aa40cf
khalil.hajri <khalil.hajri@esen.tn>
2025-03-13 22:34:29 +0100

    initialize app

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
+
+app.get('/', (req, res) => {
+  res.send('Barber Booking App');
+});
+
+module.exports = app;
