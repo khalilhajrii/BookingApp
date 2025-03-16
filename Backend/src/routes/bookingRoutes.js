78ed753b77a1c1dfa19376185fbee70a08e53d99
khalil.hajri <khalil.hajri@esen.tn>
2025-03-16 14:00:39 +0100

    implement authorization

--- Backend/src/routes/bookingRoutes.js
+++ Backend/src/routes/bookingRoutes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
 const checkRole = require('../middleware/checkRole');
+const jwtMiddleware = require('../middleware/authMiddleware');
 
-router.post('/bookings', checkRole('user'), bookingController.createBooking);
-router.put('/bookings/:id', checkRole('user'), bookingController.updateBooking);
-router.delete('/bookings/:id', checkRole('user'), bookingController.deleteBooking);
+router.post('/bookings', jwtMiddleware, checkRole('user'), bookingController.createBooking);
+router.put('/bookings/:id', jwtMiddleware, checkRole('user'), bookingController.updateBooking);
+router.delete('/bookings/:id', jwtMiddleware, checkRole('user'), bookingController.deleteBooking);
 
 // Barber routes
-router.get('/barber/bookings', checkRole('barber'), bookingController.getBarberBookings);
-router.put('/barber/bookings/:id', checkRole('barber'), bookingController.updateBookingStatus);
+router.get('/barber/bookings', jwtMiddleware, checkRole('barber'), bookingController.getBarberBookings);
+router.put('/barber/bookings/:id', jwtMiddleware, checkRole('barber'), bookingController.updateBookingStatus);
 
 module.exports = router;
